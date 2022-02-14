8d6822426b6e910d332c38467eed14c541ddd00f
karluze <karluzem@gmail.com>
2022-02-14 16:21:11 -0300

    Projeto 1016

---
+++ Projeto 1016/Projeto 1016/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int KM, resultado;
+
+            KM = int.Parse(Console.ReadLine());
+
+            resultado = KM * 2;
+
+            Console.WriteLine(resultado + " minutos");
+
+            //Console.ReadLine();
+        }
+    }
+}
